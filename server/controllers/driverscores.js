8c7b42d8dd3a416c0cec49d87ebdebf197fcf770
Federico Longhi <federico.rlonghi@gmail.com>
2019-05-25 19:12:27 -0300

    agrego actualizacion del driver cuando se sube un score

--- server/controllers/driverscores.js
+++ server/controllers/driverscores.js
@@ -1,4 +1,5 @@
 const DriverScore = require('../models').DriverScore;
+const Driver = require('../models').Driver;
 
 module.exports = {
     create(req, res) {
@@ -10,7 +11,20 @@ module.exports = {
             value: req.body.value,
             comments: req.body.comments
         })
-        .then(driverScore => res.status(201).send(driverScore))
+        .then(driverScore => {
+            Driver.findByPk(driverScore.toId)
+            .then(driver => {
+                var newTotalScore = driver.totalScore + driverScore.value;
+                var newScoreQuantity = driver.scoreQuantity + 1;
+                driver.update({
+                    totalScore: newTotalScore,
+                    scoreQuantity: newScoreQuantity
+                })
+                res.status(201).send(driverScore);
+            })
+            .catch(error => res.status(400).send(error));
+            //res.status(201).send(driverScore)
+        })
         .catch(error => res.status(400).send(error));
     },
 
