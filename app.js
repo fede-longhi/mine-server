a3bd1150a3969c3150c5b8d52ab064b497262de1
rubenJimenez33 <rbnm.jimenez@gmail.com>
2019-06-01 21:23:17 -0300

    adding cors

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const logger = require('morgan');
 const bodyParser = require('body-parser');
 require("custom-env").env("pmm");
+const cors = require('cors');
 
 const distance = require('google-distance-matrix');
 distance.key(process.env.GOOGLE_API_KEY);
 distance.mode('driving');
 
 const app = express();
+
+//Enable Alls CORS Request
+app.use(cors());
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
