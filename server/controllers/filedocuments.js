774f82d7ac0ae8cfe630f04633cb49400b4a6720
hernan <hernanfiuba2009@gmail>
2019-06-06 03:30:01 -0300

    change retrive FileDocuments endpoint: partyId by userId/driverId, and added new parameter called name

--- server/controllers/filedocuments.js
+++ server/controllers/filedocuments.js
@@ -1,5 +1,6 @@
 const FileDocument = require('../models').FileDocument;
-
+const User = require('../models').User;
+const Driver = require('../models').Driver;
 module.exports = {
     create(req, res) {
         console.log('Request create FileDocument');
@@ -16,57 +17,144 @@ module.exports = {
         .catch(error => res.status(400).send(error));
     },
 
-    retrieve(req, res) {
-        console.log('Request find by query params FileDocument: ' + JSON.stringify(req.query));
-        if (Object.keys(req.query).length > 0) {
-            if (req.query.fileDocumentId != null && req.query.fileDocumentId != undefined && req.query.partyId != null && req.query.partyId!= undefined) {
-                return FileDocument
-                .findByPk(req.query.fileDocumentId)
-                .then(fileDocument => {
-                    if (!!fileDocument && fileDocument.partyId == req.query.partyId) {
-                        return res.status(200).send(fileDocument);  
-                    }
+    retriveByQueryParams(req, res, partyId) {
+        console.log("retriveByQueryParams");
+        if (req.query.fileDocumentId != null && req.query.fileDocumentId != partyId != null) {
+            return FileDocument
+            .findByPk(req.query.fileDocumentId)
+            .then(fileDocument => {
+                if (!!fileDocument && fileDocument.partyId == req.query.partyId) {
+                    return res.status(200).send(fileDocument);  
+                }
+                return res.status(400).send({
+                    message: "FileDocument not found."
+                })
+            })
+            .catch(error => res.status(400).send(error));
+        }
+        else if (req.query.fileDocumentId != null && req.query.fileDocumentId != undefined) {
+            return FileDocument
+            .findByPk(req.query.fileDocumentId)
+            .then(fileDocument => {
+                if (!fileDocument) {
                     return res.status(400).send({
                         message: "FileDocument not found."
                     })
-                })
-                .catch(error => res.status(400).send(error));
-            }
-            else if (req.query.fileDocumentId != null && req.query.fileDocumentId != undefined) {
-                return FileDocument
-                .findByPk(req.query.fileDocumentId)
-                .then(fileDocument => {
-                    if (!fileDocument) {
-                        return res.status(400).send({
-                            message: "FileDocument not found."
-                        })
-                    }
-                    return res.status(200).send(fileDocument);
-                })
-                .catch(error => res.status(400).send(error));
-            } else if (req.query.partyId != null && req.query.partyId!= undefined) {
-                return FileDocument
-                .findAll({
-                    where: {
-                        partyId: req.query.partyId
-                    }
-                })
-                .then(fileDocument => {
-                    if (!fileDocument) {
-                        return res.status(400).send({
-                            message: "FileDocument not found."
-                        })
-                    }
-                    return res.status(200).send(fileDocument);
-                })
-                .catch(error => res.status(400).send(error));
+                }
+                return res.status(200).send(fileDocument);
+            })
+            .catch(error => res.status(400).send(error));
+        } else if (req.query.partyId != null && req.query.partyId!= undefined) {
+            return FileDocument
+            .findAll({
+                where: {
+                    partyId: req.query.partyId
+                }
+            })
+            .then(fileDocument => {
+                if (!fileDocument) {
+                    return res.status(400).send({
+                        message: "FileDocument not found."
+                    })
+                }
+                return res.status(200).send(fileDocument);
+            })
+            .catch(error => res.status(400).send(error));
+        }
+    },
+
+    retrieve(req, res) {
+        console.log('Request find by query params FileDocument: ' + JSON.stringify(req.query));
+        if (Object.keys(req.query).length > 0) {
+            if (req.query.userId != null && req.query.userId != undefined) {
+                User.findByPk(req.query.userId)
+                    .then(user => {
+                        if (!user) {
+                            return res.status(400).send({
+                                message: "User not found."
+                            })
+                        }
+                        if (req.query.fileDocumentId != null && req.query.fileDocumentId != user.partyId != null) {
+                            return FileDocument
+                                .findByPk(req.query.fileDocumentId)
+                                .then(fileDocument => {
+                                    if (!!fileDocument && fileDocument.partyId == user.partyId) {
+                                        return res.status(200).send(fileDocument);  
+                                    }
+                                    return res.status(400).send({message: "FileDocument not found."})
+                                })
+                                .catch(error => res.status(400).send(error));
+                        } else {
+                            return FileDocument
+                                    .findAll({where: {partyId: user.partyId}})
+                                    .then(fileDocuments => {
+                                        var fileDocumentAux = fileDocuments;                                        
+                                        if (!fileDocuments) {
+                                            return res.status(400).send({message: "FileDocument not found."})
+                                        }
+                                        if (req.query.name != null && req.query.name != undefined) {
+                                            fileDocumentAux = fileDocuments.filter(fd => fd.name == req.query.name);
+                                        }
+                                        return res.status(200).send(fileDocumentAux);
+                                    })
+                                    .catch(error => res.status(400).send(error));
+                        }
+                    })
+                    .catch(error => res.status(400).send(error));
+            } else if (req.query.driverId != null && req.query.driverId != undefined) {
+                Driver.findByPk(req.query.driverId)
+                    .then(driver => {
+                        if (!driver) {
+                            return res.status(400).send({message: "Driver not found."})
+                        }
+                        if (req.query.fileDocumentId != null && req.query.fileDocumentId != driver.partyId != null) {
+                            return FileDocument
+                                    .findByPk(req.query.fileDocumentId)
+                                    .then(fileDocument => {
+                                        if (!!fileDocument && fileDocument.partyId == driver.partyId) {
+                                            return res.status(200).send(fileDocument);  
+                                        }
+                                        return res.status(400).send({
+                                            message: "FileDocument not found."
+                                        })
+                                    })
+                                    .catch(error => res.status(400).send(error));
+                        } else {
+                            return FileDocument
+                                    .findAll({where: {partyId: driver.partyId}})
+                                    .then(fileDocuments => {
+                                        var fileDocumentAux = fileDocuments;                                        
+                                        if (!fileDocuments) {
+                                            return res.status(400).send({message: "FileDocument not found."})
+                                        }
+                                        if (req.query.name != null && req.query.name != undefined) {
+                                            fileDocumentAux = fileDocuments.filter(fd => fd.name == req.query.name);
+                                        }
+                                        return res.status(200).send(fileDocumentAux);
+                                    })
+                                    .catch(error => res.status(400).send(error));
+                        }
+                    })
+                    .catch(error => res.status(400).send(error));
+            } else if (req.query.fileDocumentId != null && req.query.fileDocumentId != undefined) {
+                    return FileDocument
+                            .findByPk(req.query.fileDocumentId)
+                            .then(fileDocument => {
+                                if (!fileDocument) {
+                                    return res.status(400).send({message: "FileDocument not found."})
+                                }
+                                return res.status(200).send(fileDocument);
+                            })
+                            .catch(error => res.status(400).send(error));
+            } else {
+                return res.status(400).send({message: "FileDocument not found."})
             }
         } else {
             console.log('Find all');
             return FileDocument
-                .findAll()
-                .then((fileDocument) => res.status(200).send(fileDocument))
-                .catch(error => res.status(400).send(error));
+                    .findAll()
+                    .then((fileDocument) => res.status(200).send(fileDocument))
+                    .catch(error => res.status(400).send(error));
         }
     },
 
